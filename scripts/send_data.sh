171f80d6db55568976b56387ca35f69771c919eb
Rafael Faita <rfaita@DESKTOP-O4B4QH6.localdomain>
2021-05-19 16:07:45 -0300

    removing unused scripts

--- scripts/send_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-i=383200
-while true; do
-    MESSAGE="{\"auctionId\": $i}"
-    echo $MESSAGE
-    rabbitmqadmin -u rfaita -p M@inpassword publish routing_key='auction' properties='{"content_type":"application/json"}' payload="$MESSAGE" payload_encoding='string'
-    i=$(($i-1))
-    sleep .5
-done
